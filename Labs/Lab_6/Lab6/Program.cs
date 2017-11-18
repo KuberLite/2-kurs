d4361a958f7cec3b15b87482325dc1a71d8283c5
Denis Grahovskiy <kuberlite@gmail.com>
2017-11-18 18:43:51 +0300

    Lab_6 complete

---
+++ Labs/Lab_6/Lab6/Program.cs
@@ -0,0 +1,124 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Создать Планету Земля. 
+ *Найти все государства на заданном континенте. 
+ *Подсчитать количество морей.
+ *Вывести острова по алфавиту.*/
+
+namespace Lab6
+{
+    enum NameState { Canada, USA, Mexico, Cuba, Panama, Jamaica }
+
+    interface IOcean<T>
+    { 
+        string ToSence();
+    }
+    class Sea : IOcean<Sea>
+    {
+        public string ToSence()
+        {
+            return "Poland forever";
+        }
+    }
+    class Island : IOcean<Island>
+    {
+        private static int objCount = 0;
+        private readonly string name;
+        private int count;
+        private static Random randNum = new Random();
+
+        public Island()
+        {
+            objCount++;
+            name = "Island" + objCount;
+            GenerateIsland();
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+        public int getCount()
+        {
+            return count;
+        }
+        public void GenerateIsland()
+        {
+            count = randNum.Next(1, 1000);
+        }
+        public string ToSence()
+        {
+            return "island eee";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Message add = new Message();    
+            Message add2 = new Message();  
+            for(int i = 0; i < 50; i++)
+            {
+                add.Add();
+            }
+
+            for(int i = 0; i < 300; i++)
+            {
+                add2.Add();
+            }
+            int z = 0;
+            int buffer = 0;
+            do
+            {
+                Console.Clear();
+                Console.Write("1. Add Element\n2. Delete Element\n3. Print all name of island\n4. How many countries on the continent\n5. Print number of seas\n0. Exit\n");
+                Console.Write("#");
+                z = Convert.ToInt32(Console.ReadLine());
+                switch (z)
+                {
+                    default:
+                        Console.Write("Error: wrong number\n");
+                        Console.ReadKey();
+                        break;
+                    case 1:
+                        add.Add();
+                        Console.WriteLine("One sea added");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the item you want to delete");
+                        buffer = Convert.ToInt32(Console.ReadLine());
+                        add.DeleteAt(buffer);
+                        Console.WriteLine("Element number" + buffer + "was deleted");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.WriteLine("Names of all islands\n");
+                        add.PrintNamesIsland();
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.WriteLine("Countries on the Continent");
+                        str = Console.ReadLine();
+                        add.PrintStateInContinent(str);
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        Console.Write("\nThere is " + add.getNumberOfSeas() + " seas in the world");
+                        Console.ReadKey();
+                        break;
+                    case 0:
+                        break;
+                }
+            } while (z != 0);
+
+        }
+    }
+}
---
+++ Labs/Lab_7/Lab6/ClassCount.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+
+    struct Area
+    {
+        public Sea[] sea;
+        public Island island;
+    }
+
+
+    class Message
+    {
+        private List<Area> sea_sea;
+
+        public Message()
+        {
+            sea_sea = new List<Area>();
+        }
+
+        public void PrintStateInContinent(string str)
+        {
+            if (str == "Australia")
+            {
+                Console.WriteLine("Australia");
+            }
+            else if (str == "Eurasia")
+            {
+                Console.WriteLine("######\n China\n Russia\n Belarus\n France\n Germany\n Italy\n Korea\n India\n Iran\n Greece");
+            }
+            else if (str == "North America")
+            {
+                Console.WriteLine("######\n USA\n Mexica\n Canada\n Cuba\n Jamaica\n San-Marino\n Panama");
+            }
+            else { Console.WriteLine("Wrong Continent"); }
+        }        
+
+        public void Add()
+        {
+            Area obj = new Area();
+            obj.sea = new Sea[9];
+            for (int i = 0; i < 9; i++)
+            {
+                obj.sea[i] = new Sea();
+            }
+            obj.island = new Island();
+            sea_sea.Add(obj);
+        }
+
+
+        public int getNumberOfSeas()
+        {
+            return sea_sea.Count;
+        }
+
+        public string foundState()
+        {
+            return "lol";
+        }
+
+        public void DeleteAt(int index)
+        {
+            if (index < 0 || index > sea_sea.Count)
+                return;
+            sea_sea.RemoveAt(index);
+        }
+
+        public void PrintNamesIsland()
+        {
+            Console.Write("All names of island:\n");
+            foreach (var i in sea_sea)
+            {
+                Console.Write(" - " + i.island.getName() + "\n");
+            }
+        }
+    }
+}
