4d22023d4797c85a726fd6b4f08c992e17640d5b
Denis Grahovskiy <kuberlite@gmail.com>
2017-11-20 02:07:29 +0300

    Lab_7 Complete

---
+++ Labs/Lab_7/Lab6/PersonalDllNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class PersonalDllNotFoundException : DllNotFoundException
+    {
+        public PersonalDllNotFoundException(string name) : base(name)
+        {
+
+        }
+    }
+}
--- Labs/Lab_7/Lab6/Program.cs
+++ Labs/Lab_7/Lab6/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 /*Создать Планету Земля. 
  *Найти все государства на заданном континенте. 
@@ -59,7 +60,7 @@ namespace Lab6
 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             string str;
             Message add = new Message();    
@@ -80,45 +81,131 @@ namespace Lab6
                 Console.Clear();
                 Console.Write("1. Add Element\n2. Delete Element\n3. Print all name of island\n4. How many countries on the continent\n5. Print number of seas\n0. Exit\n");
                 Console.Write("#");
-                z = Convert.ToInt32(Console.ReadLine());
-                switch (z)
+
+                try
                 {
-                    default:
-                        Console.Write("Error: wrong number\n");
-                        Console.ReadKey();
-                        break;
-                    case 1:
-                        add.Add();
-                        Console.WriteLine("One sea added");
-                        Console.ReadKey();
-                        break;
-                    case 2:
-                        Console.WriteLine("Enter the item you want to delete");
-                        buffer = Convert.ToInt32(Console.ReadLine());
-                        add.DeleteAt(buffer);
-                        Console.WriteLine("Element number" + buffer + "was deleted");
-                        Console.ReadKey();
-                        break;
-                    case 3:
-                        Console.WriteLine("Names of all islands\n");
-                        add.PrintNamesIsland();
-                        Console.ReadKey();
-                        break;
-                    case 4:
-                        Console.WriteLine("Countries on the Continent");
-                        str = Console.ReadLine();
-                        add.PrintStateInContinent(str);
-                        Console.ReadKey();
-                        break;
-                    case 5:
-                        Console.Write("\nThere is " + add.getNumberOfSeas() + " seas in the world");
-                        Console.ReadKey();
-                        break;
-                    case 0:
-                        break;
+                    z = Convert.ToInt32(Console.ReadLine());
+
                 }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message + "\nPress any key to continue...");
+                    Console.ReadKey();
+                    Main();
+                }
+                finally
+                {
+
+                }
+
+                switch (z)
+                    {
+                            default:
+                                Console.Write("Error: wrong number\n");
+                                Console.ReadKey();
+                                break;
+                            case 1:
+                                add.Add();
+                                Console.WriteLine("One sea added");
+                                Console.ReadKey();
+                                break;
+                            case 2:
+                            {
+                                Console.WriteLine("Enter the item you want to delete");
+                                try
+                                {
+                                    buffer = Convert.ToInt32(Console.ReadLine());
+                                    add.DeleteAt(buffer);
+                                    Console.WriteLine("Element number " + buffer + " was deleted");
+                                    Console.ReadKey();
+                                }
+                                catch(Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message + "\nPress any key to continue...");
+                                    Console.ReadKey();
+                                }                        
+                            
+                                break;
+                            }
+                            case 3:
+                                Console.WriteLine("Names of all islands\n");
+                                add.PrintNamesIsland();
+                                Console.ReadKey();
+                                break;
+                            case 4:
+                            {
+                                Console.WriteLine("Countries on the Continent");
+                                try
+                                {
+                                    str = Console.ReadLine();
+                                    add.PrintStateInContinent(str);
+                                    Console.ReadKey();
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+                                finally
+                                {
+
+                                }
+                                break;
+                            }
+                            case 5:
+                                Console.Write("\nThere is " + add.getNumberOfSeas() + " seas in the world");
+                                Console.ReadKey();
+                                break;
+                            case 6:
+                                int c = task();
+                                Debug.Assert(c < 10, "Eroooooor");
+                                Console.WriteLine(c);
+                                Console.ReadKey();
+                            break;
+
+                            case 0:
+                                break;
+                    }
             } while (z != 0);
 
+        }
+
+        static int task()
+        {
+            int a = 0, b = 0, c = 0;
+            try
+            {
+                Console.WriteLine("Enter 1 number(>3): ");
+                a = Convert.ToInt32(Console.ReadLine());
+                if (a < 3)
+                {
+                    throw new PersonalException("Enter number more 3");
+                }
+                Console.WriteLine("Enter 2 number(=0): ");
+
+                b = Convert.ToInt32(Console.ReadLine());
+                if (b == 0)
+                {
+                    throw new PersonalDivideByZeroException("Enter not a zero");
+                }
+                c = a / b;
+            }
+            catch (PersonalDivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                task();
+                Console.ReadKey();
+            }   
+            catch (PersonalException ex)
+            {
+                Console.WriteLine(ex.Message);
+                task();
+                Console.ReadKey();
+            }
+            finally
+            {
+
+            }
+            return c;
         }
     }
 }
