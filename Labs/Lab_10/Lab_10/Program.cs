d421a040756c4b387c0e73ffc53fea63f279dc97
Denis Grahovskiy <kuberlite@gmail.com>
2017-12-03 00:17:39 +0300

    Lab_10 Complete

---
+++ Labs/Lab_10/Lab_10/Program.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Lab_10
+{
+    class Person
+    {
+        public string Country;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int del, caseSwitch = 0, searchItem, delItem, searchSorted;
+            ArrayList arraylist = new ArrayList();
+            Random random = new Random();
+            Queue<int> queue = new Queue<int>();
+            do
+            {
+                Console.Clear();
+                Console.Write("1. ArrayList\n2. Queue|SortedDictionary\n3. SortedDictionary with class\n4. ObservableCollection\n0. Exit\n");
+                Console.Write(">>");
+                caseSwitch = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (caseSwitch)
+                {
+                    default:
+                        Console.Write("Error: wrong number\n");
+                        Console.Write("\nPress any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 1:
+                        for (int i = 0; i < 5; i++)
+                        {
+                            arraylist.Add(random.Next(15, 100));
+                        }
+
+                        arraylist.Add("ArrayList");
+
+                        Console.WriteLine(arraylist.Count + " elements");
+
+                        Console.Write("{ ");
+                        foreach (object o in arraylist)
+                        {
+                            Console.Write(o + " ");
+                        }
+                        Console.WriteLine("}");
+
+                        Console.WriteLine("Delete element number: ");
+                        del = Convert.ToInt32(Console.ReadLine());
+                        arraylist.RemoveAt(del);
+
+                        Console.WriteLine("------New list------");
+
+                        Console.WriteLine(arraylist.Count + " elements");
+
+                        Console.Write("{ ");
+                        foreach (object o in arraylist)
+                        {
+                            Console.Write(o + " ");
+                        }
+                        Console.WriteLine("}");
+
+                        Console.WriteLine("Enter the item number to search: ");
+
+                        searchItem = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("It is " + arraylist[searchItem]);
+
+                        Console.Write("\nPress any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        for (int i = 0; i < 10; i++)
+                        {
+                            queue.Enqueue(random.Next(50, 100));
+                        }
+
+                        Console.WriteLine(queue.Count + " elements");
+
+                        Console.Write("{ ");
+
+                        foreach (int num in queue)
+                        {
+                            Console.Write(num + " ");
+                        }
+                        Console.WriteLine("}");
+
+                        Console.WriteLine();
+
+                        Console.WriteLine("Enter the number of items to delete: ");
+                        delItem = Convert.ToInt32(Console.ReadLine());
+
+                        for (int i = 0; i < delItem; i++)
+                        {
+                            queue.Dequeue();
+                        }
+
+                        Console.Write("{ ");
+
+                        foreach (int num in queue)
+                        {
+                            Console.Write(num + " ");
+                        }
+                        Console.WriteLine("}");
+
+                        SortedDictionary<int, int> sortedDictionary = new SortedDictionary<int, int>();
+                        int k = 0;
+                        foreach (int num in queue)
+                        {
+                            sortedDictionary.Add(k, num);
+                            k++;
+                        }
+
+                        Console.WriteLine("--------sortedDictionary------");
+
+                        Console.Write("{ ");    
+                        for (int i = 0; i < sortedDictionary.Count; i++)
+                        {
+                            Console.Write(sortedDictionary[i] + " ");
+                        }
+
+                        Console.WriteLine("}");
+
+                        Console.WriteLine("Enter the item number to search: ");
+                        searchSorted = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Your number: " + sortedDictionary[searchSorted]);
+                        Console.Write("\nPress any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        int delCountry, searchSortedCountry;
+                        Queue<Person> queuePerson = new Queue<Person>();
+                        queuePerson.Enqueue(new Person() { Country = "Belarus" });
+                        queuePerson.Enqueue(new Person() { Country = "Russia" });
+                        queuePerson.Enqueue(new Person() { Country = "Poland" });
+                        queuePerson.Enqueue(new Person() { Country = "Ukraine" });
+                        queuePerson.Enqueue(new Person() { Country = "France" });
+                        queuePerson.Enqueue(new Person() { Country = "UK" });
+                        queuePerson.Enqueue(new Person() { Country = "USA" });
+                        queuePerson.Enqueue(new Person() { Country = "Greece" });
+
+                        Console.WriteLine(queuePerson.Count + " elements");
+
+                        Console.Write("{ ");
+
+                        foreach (Person num in queuePerson)
+                        {
+                            Console.Write(num.Country + " ");
+                        }
+                        Console.WriteLine("}");
+
+                        Console.WriteLine();
+
+                        Console.WriteLine("Enter the number of country to delete: ");
+                        delCountry = Convert.ToInt32(Console.ReadLine());
+
+                        for (int i = 0; i < delCountry; i++)
+                        {
+                            queuePerson.Dequeue();
+                        }
+
+                        Console.Write("{ ");
+
+                        foreach (Person num in queuePerson)
+                        {
+                            Console.Write(num.Country + " ");
+                        }
+                        Console.WriteLine("}");
+
+                        Console.WriteLine();
+
+                        SortedDictionary<int, Person> sortedDictionaryPerson = new SortedDictionary<int, Person>();
+                        int countPerson = 0;
+                        foreach (Person num in queuePerson)
+                        {
+                            sortedDictionaryPerson.Add(countPerson, num);
+                            countPerson++;
+                        }
+
+                        Console.WriteLine("--------sortedDictionary------");
+
+                        Console.Write("{ ");
+                        for (int i = 0; i < sortedDictionaryPerson.Count; i++)
+                        {
+                            Console.Write(sortedDictionaryPerson[i].Country + " ");
+                        }
+                        Console.WriteLine("}");
+                        Console.WriteLine("Enter the item number to search: ");
+                        searchSortedCountry = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Your number: " + sortedDictionaryPerson[searchSortedCountry].Country);
+                        Console.Write("\nPress any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        ObservableCollection<Person> person = new ObservableCollection<Person>
+                        {
+                            new Person { Country = "Belarus"},
+                            new Person { Country = "Russia"},
+                            new Person { Country = "Poland"}
+                        };
+
+                        person.CollectionChanged += Persons_CollectionChanged;
+
+                        person.Add(new Person { Country = "Ukraine" });
+                        person.RemoveAt(1);
+                        person[0] = new Person { Country = "Greece" };
+
+                        foreach (Person persons in person)
+                        {
+                            Console.WriteLine(persons.Country);
+                        }
+                        Console.Write("\nPress any key to continue...");
+                        Console.ReadKey();              
+                        break;
+                    case 0:
+                        break;
+                }
+            } while (caseSwitch != 0);
+        }
+
+        private static void Persons_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add: // если добавление
+                    Person newUser = e.NewItems[0] as Person;
+                    Console.WriteLine("Added new object: {0}", newUser.Country);
+                    break;
+                case NotifyCollectionChangedAction.Remove: // если удаление
+                    Person oldUser = e.OldItems[0] as Person;
+                    Console.WriteLine("Deleted object: {0}", oldUser.Country);
+                    break;
+                case NotifyCollectionChangedAction.Replace: // если замена
+                    Person replacedUser = e.OldItems[0] as Person;
+                    Person replacingUser = e.NewItems[0] as Person;
+                    Console.WriteLine("Object {0} replaced by an object {1}",
+                                        replacedUser.Country, replacingUser.Country);
+                    break;
+            }
+        }
+    }
+}
---
+++ Labs/Lab_9/Lab_9/Program.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9 {
+    class Programmer {
+        ProgramLanguage[] PL;
+        public event EventHandler<ProgramLanguage> pl = null;
+        public Action action = null;
+
+        public Programmer() {
+            PL = new ProgramLanguage[10];
+            for (int i = 0; i < PL.Length; i++) {
+                PL[i] = new ProgramLanguage();
+            }
+        }
+
+        public void Rename(int index, string str) {
+            if (pl == null)
+                return;
+
+            action = () => PL[index].name = str; // замыкание
+            pl(this, PL[index]); // событие
+        }
+
+        public void AddProperties(int index, string[] str) {
+            if (pl == null)
+                return;
+
+            action = () => PL[index].properties = str; // замыкание
+            pl(this, PL[index]); // событие
+        }
+
+        public override string ToString() {
+            string str = "";
+            for (int i = 0; i < PL.Length; i++) {
+                str += i + ". " + PL[i].name;
+                for (int j = 0; j < PL[i].propertiesCount; j++) {
+                    str += "\n > " + PL[i].properties[j];
+                }
+                str += "\n";
+            }
+
+            return str;
+        }
+    }
+
+    class ProgramLanguage : EventArgs {
+        public string name { get; set; }
+        public int propertiesCount { get; private set; }
+        private string[] Properties = null;
+        public string[] properties {
+            get {
+                return Properties;
+            }
+            set {
+                if (100 <= propertiesCount + value.Length) {
+                    throw new IndexOutOfRangeException("cannot add new item");
+                }
+                foreach (var i in value) {
+                    Properties[propertiesCount++] = i;
+                }
+            }
+        }
+        private static int objCount = -1;
+
+        public ProgramLanguage() {
+            objCount++;
+            name = "С";
+            propertiesCount = 0;
+            for (int i = 0; i < objCount; i++) {
+                name += "+";
+            }
+            Properties = new string[100];
+            properties = new string[] { "C based language" };
+        }
+    }
+
+    class SomeClass {
+        public static string SplitWordsByComma(string str) {
+            char[] arr = str.ToCharArray();
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++) {
+                if (arr[i] == ' ') {
+                    count++;
+                }
+            }
+            char[] narr = new char[arr.Length + count + 1];
+            for (int i = 0, j = 0; i < arr.Length; i++, j++) {
+                if (arr[i] == ' ') {
+                    narr[j] = ',';
+                    j++;
+                }
+                narr[j] = arr[i];
+            }
+
+            str = new string(narr);
+            Console.WriteLine("Output: " + str);
+            return str;
+        }
+
+        public static string DeleteDots(string str) {
+            char[] arr = str.ToCharArray();
+            int length = arr.Length;
+
+            for (int i = 0; i < length; i++) {
+                if (arr[i] == '.') {
+                    for (int j = i; j < arr.Length - 1; j++) {
+                        arr[j] = arr[j + 1];
+                    }
+                    length--;
+                }
+            }
+
+            char[] narr = new char[length];
+            for (int i = 0; i < length; i++) {
+                narr[i] = arr[i];
+            }
+
+            str = new string(narr);
+            Console.WriteLine("Output: " + str);
+            return str;
+        }
+
+        public static string ToUpperCase(string str) {
+            Console.WriteLine("Output: " + str.ToUpper());
+            return str.ToUpper();
+        }
+
+        public static string DeleteColons(string str) {
+            char[] arr = str.ToCharArray();
+            int length = arr.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (arr[i] == ':')
+                {
+                    for (int j = i; j < arr.Length - 1; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                    }
+                    length--;
+                }
+            }
+
+            char[] narr = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                narr[i] = arr[i];
+            }
+
+            str = new string(narr);
+            Console.WriteLine("Output: " + str);
+            return str;
+        }
+
+        public static string DeleteSemicolons(string str) {
+            char[] arr = str.ToCharArray();
+            int length = arr.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (arr[i] == ';')
+                {
+                    for (int j = i; j < arr.Length - 1; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                    }
+                    length--;
+                }
+            }
+
+            char[] narr = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                narr[i] = arr[i];
+            }
+
+            str = new string(narr);
+            Console.WriteLine("Output: " + str);
+            return str;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Programmer programmer = new Programmer();
+            programmer.pl += delegate (object sender, ProgramLanguage pl) {
+                Programmer d = (Programmer)sender;
+                d.action(); // вызов замыкания
+                string str = new string('-', 40);
+                str += "\n";
+                str += pl.name;
+                for (int j = 0; j < pl.propertiesCount; j++) {
+                    str += "\n > " + pl.properties[j];
+                }
+                Console.WriteLine("\n" + str);
+            };
+
+            int z = 0, buffer1 = 0, buffer2 = 0;
+            string[] buffer;
+            string TestString = "";
+
+            Func<string, string> f = null;
+
+            do {
+                Console.Clear();
+                Console.Write("1. Rename\n2. Add properties\n3. Task2\n0. Exit\n");
+                Console.Write(">/");
+                try {
+                    z = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException) {
+                    Console.WriteLine("\nError: FormatException");
+                    Console.Write("\nPress any key to continue...");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+                switch (z) {
+                    default:
+                        Console.Write("Error: wrong number\n");
+                        Console.Write("\nPress any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 1:
+                        Console.WriteLine(programmer.ToString());
+                        try {
+                            Console.Write("Enter index: ");
+                            buffer1 = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter number of properties: ");
+                        }
+                        catch (FormatException) {
+                            Console.WriteLine("\nError: FormatException");
+                            Console.Write("\nPress any key to continue...");
+                            Console.ReadKey();
+                        }
+                        buffer = new string[1];
+                        Console.Write(" >Enter new name: ");
+                        buffer[0] = Console.ReadLine();
+                        programmer.Rename(buffer1, buffer[0]);
+                        Console.Write("\nPress any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine(programmer.ToString());
+                        try {
+                            Console.Write("Enter index: ");
+                            buffer1 = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter number of properties: ");
+                            buffer2 = Convert.ToInt32(Console.ReadLine());
+                            if (buffer2 <= 0)
+                                throw new FormatException("must be positive");
+                        }
+                        catch (FormatException) {
+                            Console.WriteLine("\nError: FormatException");
+                            Console.Write("\nPress any key to continue...");
+                            Console.ReadKey();
+                        }
+                        buffer = new string[buffer2];
+                        for (int i = 0; i < buffer2; i++) {
+                            Console.Write(" >Enter property " + i + ": ");
+                            buffer[i] = Console.ReadLine();
+                        }
+                        programmer.AddProperties(buffer1, buffer);
+                        Console.Write("\nPress any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.Write("Enter string: ");
+                        TestString = Console.ReadLine();
+
+                        f = SomeClass.ToUpperCase;
+                        TestString = f(TestString);
+                        f = SomeClass.DeleteDots;
+                        TestString = f(TestString);
+                        f = SomeClass.DeleteColons;
+                        TestString = f(TestString);
+                        f = SomeClass.DeleteSemicolons;
+                        TestString = f(TestString);
+                        f = SomeClass.SplitWordsByComma;
+                        TestString = f(TestString);
+
+                        Console.WriteLine("\nResult: " + TestString);
+                        Console.Write("\nPress any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 0:
+                        Console.Write("\nPress any key to continue...");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (z != 0);
+        }
+    }
+}
