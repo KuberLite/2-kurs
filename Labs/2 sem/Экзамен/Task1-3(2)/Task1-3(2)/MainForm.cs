156c0b9f646177b99e6b23b0d6451c20abf53da9
KuberLite <31598215+KuberLite@users.noreply.github.com>
2018-06-27 22:15:20 +0300

    added 1-3/2

---
+++ Labs/2 sem/Экзамен/Task1-3(2)/Task1-3(2)/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Task1_3_2_
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmPassord()
+        {
+            Regex regex = new Regex(@"^((?=.*\d)(?=.*[a-z])(?=.*[@#$%]).{6,12})");
+            MatchCollection matches = regex.Matches(PassTextBox.Text.Trim());
+            if (matches.Count > 0)
+            {
+                label1.Text = "OK";
+            }
+            else
+            {
+                label1.Text = "not ok";
+            }
+        }
+
+        private void ConfirmButton_Click(object sender, EventArgs e)
+        {
+            ConfirmPassord();
+            if (PassTextBox.Text == ConfirmPassTextBox.Text) MessageBox.Show("Good");
+            else MessageBox.Show("Not good");
+            
+        }
+    }
+}
